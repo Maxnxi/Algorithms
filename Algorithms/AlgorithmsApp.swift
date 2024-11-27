370608b8cba4610ffeb8a795bef33dc0c448c4ec
Maxnxi <nisq@ya.ru>
2024-11-27 11:08:06 +0700

    Initial Commit

---
+++ Algorithms/AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsApp.swift
+//  Algorithms
+//
+//  Created by Maksim Ponomarev on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
