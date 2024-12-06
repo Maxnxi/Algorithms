dc6cc82d32202ebf5dd3e5fe8e14dc65be19fac5
Maxnxi <nisq@ya.ru>
2024-12-06 21:03:20 +0700

    add Generating permutations

--- Algorithms/Flow/Algorithms/Part_1_Simple_Algorithms/2.Generating subsets/GeneratingSubsets.swift
+++ Algorithms/Flow/Algorithms/Part_1_Simple_Algorithms/2.Generating subsets/GeneratingSubsets.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+func generateSubsets(index: Int, subset: inout [Int], nums: [Int]) {
+	// Print the current subset
+	print("{ \(subset.map(String.init).joined(separator: " ")) }")
+	
+	// Generate subsets starting from the current index
+	for i in index..<nums.count {
+		// Include nums[i] in the current subset
+		subset.append(nums[i])
+		// Recur for the next elements
+		generateSubsets(index: i + 1, subset: &subset, nums: nums)
+		// Backtrack to explore other possibilities
+		subset.removeLast()
+	}
+}
+
+/*
+Example:
+ func main() {
+	var subset: [Int] = [] // To hold the current subset
+	let nums = [1, 2, 3] // Input set
+
+	generateSubsets(index: 0, subset: &subset, nums: nums) // Start generating subsets
+}
+
+main()
+
+*/
---
+++ Algorithms/Flow/Algorithms/Part_1_Simple_Algorithms/3.Generating permutations/GeneratingPermutations.swift
@@ -0,0 +1,8 @@
+//
+//  GeneratingPermutations.swift
+//  Algorithms
+//
+//  Created by Maksim Ponomarev on 12/6/24.
+//
+
+import Foundation
