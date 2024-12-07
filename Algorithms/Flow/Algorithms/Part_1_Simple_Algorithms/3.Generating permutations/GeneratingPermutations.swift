3e5dc5a60806013ac2544f799bedc449cc3b1405
Maxnxi <nisq@ya.ru>
2024-12-07 17:16:55 +0700

    Update GeneratingPermutations.swift

--- Algorithms/Flow/Algorithms/Part_1_Simple_Algorithms/3.Generating permutations/GeneratingPermutations.swift
+++ Algorithms/Flow/Algorithms/Part_1_Simple_Algorithms/3.Generating permutations/GeneratingPermutations.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+func generatePermutations(
+	array: inout [Int],
+	index: Int
+) -> [[Int]] {
+	// Array to hold all the permutations
+	var results = [[Int]]()
+
+	if index == array.count {
+		// Add the current permutation to the results
+		results.append(array)
+		return results
+	}
+	
+	for i in index..<array.count {
+		// Swap the current index with the loop index
+		array.swapAt(i, index)
+		
+		// Recur to generate permutations for the next index
+		results += generatePermutations(
+			array: &array,
+			index: index + 1
+		)
+		
+		// Backtrack by swapping back
+		array.swapAt(i, index)
+	}
+	
+	return results
+}
