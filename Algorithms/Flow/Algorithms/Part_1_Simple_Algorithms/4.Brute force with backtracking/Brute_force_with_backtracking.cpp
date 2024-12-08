6954368bdd3caef80cf2065a484e8db69526d404
Maxnxi <nisq@ya.ru>
2024-12-08 21:23:39 +0700

    Update Brute_force_with_backtracking.cpp

--- Algorithms/Flow/Algorithms/Part_1_Simple_Algorithms/4.Brute force with backtracking/Brute_force_with_backtracking.cpp
+++ Algorithms/Flow/Algorithms/Part_1_Simple_Algorithms/4.Brute force with backtracking/Brute_force_with_backtracking.cpp
@@ -6,3 +6,18 @@
 //
 
 #include <stdio.h>
+
+int n = 4;
+int count = 0;
+<int> col;
+
+void search(int y) {
+	if (y == n) {
+		count++;
+		return;
+	}
+	for (int x = 0; x < n; x++ ) {
+		if (col[x] || diag1[x+y] || diag2[x-y+n-1]) continue;
+		
+	}
+}
